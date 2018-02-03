5a40557b1e106617539bb056e2ef26499aa7efc5
Savin Sergey <savin@kartel.ru>
2018-02-03 17:43:12 +0300

    add logger

--- updater/main.cpp
+++ updater/main.cpp
@@ -1,10 +1,30 @@
+#include <string>
+
+#include <boost/filesystem.hpp>
+
 #include <QTextCodec>
 #include <QtWidgets/QApplication>
 
 #include "updater.h"
 
+class ManagerLogger
+{
+public:
+	ManagerLogger(boost::filesystem::path& path)
+	{
+		freopen((path/"1.log").string().c_str(), "w", stdout);
+		freopen((path/"2.log").string().c_str(), "w", stderr);
+	}
+	~ManagerLogger()
+	{
+		fclose(stdout);
+		fclose(stderr);
+	}
+};
+
 int main(int argc, char *argv[])
 {
+	ManagerLogger logger(boost::filesystem::system_complete(argv[0]));
     // For correct UTF-8 strings interpretation.
     QTextCodec * codec = QTextCodec::codecForName("UTF-8");
     if(nullptr != codec)
